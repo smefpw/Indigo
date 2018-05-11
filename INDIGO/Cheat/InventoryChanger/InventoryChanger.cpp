44acfb537203052385c67dc6035c1f14766dbddd
Jozkah <38704522+Jozkah@users.noreply.github.com>
2018-05-11 21:18:40 +0100

    reworked inv changer
    too lazy to look for differences sorry

--- INDIGO/Cheat/InventoryChanger/InventoryChanger.cpp
+++ INDIGO/Cheat/InventoryChanger/InventoryChanger.cpp
@@ -1,139 +1,100 @@
 #include "InventoryChanger.h"
+#include "../Skin/Skin.h"
 #include <vector>
+#include <chrono>
+#include <Windows.h>
+#include <iostream>
 
-void CInventoryChanger::PostRetrieveMessage(uint32_t* punMsgType, void* pubDest, uint32_t cubDest, uint32_t* pcubMsgSize)
+using namespace std::chrono;
+
+void AddItem(CMsgSOCacheSubscribed::SubscribedType* Object, int index, int itemDefIndex, int rarity, int paintKit, int seed, float wear, std::string name, int quality)
 {
-	uint32_t MessageType = *punMsgType & 0x7FFFFFFF;
-	if (MessageType == k_EMsgGCCStrike15_v2_MatchmakingGC2ClientHello && Settings::ProfileChanger::enabled) {
-		CMsgGCCStrike15_v2_MatchmakingGC2ClientHello Message;
-		try
-		{
-			if (!Message.ParsePartialFromArray((void*)((DWORD)pubDest + 8), *pcubMsgSize - 8))
-				return;
-		}
-		catch (...)
-		{
-			return;
-		}
-		if (Settings::ProfileChanger::level != 0) {
-			Message.set_player_level(Settings::ProfileChanger::level);
-		}
-		if (Settings::ProfileChanger::xp != 0) {
-			Message.set_player_cur_xp(Settings::ProfileChanger::xp);
-		}
-		PlayerRankingInfo* Ranking = Message.mutable_ranking();
-		Ranking->set_account_id(Interfaces::SteamUser()->GetSteamID().GetAccountID());
-		if (Settings::ProfileChanger::rank_id != 0) {
-			Ranking->set_rank_id(Settings::ProfileChanger::rank_id);
-		}
-		if (Settings::ProfileChanger::wins != 0) {
-			Ranking->set_wins(Settings::ProfileChanger::wins);
-		}
-		PlayerCommendationInfo* Commendation = Message.mutable_commendation();
-		if (Settings::ProfileChanger::cmd_friendly != 0) {
-			Commendation->set_cmd_friendly(Settings::ProfileChanger::cmd_friendly);
-		}
-		if (Settings::ProfileChanger::cmd_leader != 0) {
-			Commendation->set_cmd_leader(Settings::ProfileChanger::cmd_leader);
-		}
-		if (Settings::ProfileChanger::cmd_teaching != 0) {
-			Commendation->set_cmd_teaching(Settings::ProfileChanger::cmd_teaching);
-		}
-		if ((uint32_t)Message.ByteSize() <= cubDest - 8)
-		{
-			Message.SerializeToArray((void*)((DWORD)pubDest + 8), Message.ByteSize());
-			*pcubMsgSize = Message.ByteSize() + 8;
-		}
+	CSOEconItem Skin;
+
+	Skin.set_id(20000 + index);
+	Skin.set_account_id(Interfaces::SteamUser()->GetSteamID().GetAccountID());
+	Skin.set_def_index(itemDefIndex);
+	Skin.set_inventory(20000 + index);
+	Skin.set_origin(24);
+	Skin.set_quantity(1);
+	Skin.set_level(1);
+	Skin.set_style(0);
+	Skin.set_flags(0);
+	Skin.set_in_use(false);
+	Skin.set_original_id(0);
+	//Skin.set_rarity(rarity); // let the game decides rarity. Why? Because you don't want to fuck up like Alex did.
+	Skin.set_quality(quality);
+
+	if (name.size() > 0)
+		Skin.set_custom_name(name.data());
+
+	// Paint Kit
+	auto PaintKitAttribute = Skin.add_attribute();
+	float PaintKitAttributeValue = (float)paintKit;
+
+	PaintKitAttribute->set_def_index(6);
+
+	PaintKitAttribute->set_value_bytes(&PaintKitAttributeValue, 4);
+
+	// Paint Seed
+	auto SeedAttribute = Skin.add_attribute();
+	float SeedAttributeValue = (float)seed;
+
+	SeedAttribute->set_def_index(7);
+
+	SeedAttribute->set_value_bytes(&SeedAttributeValue, 4);
+
+	// Paint Wear
+	auto WearAttribute = Skin.add_attribute();
+	float WearAttributeValue = wear;
+
+	WearAttribute->set_def_index(8);
+
+	WearAttribute->set_value_bytes(&WearAttributeValue, 4);
+
+	// Stickers
+	for (int j = 0; j < 4; j++)
+	{
+		// Sticker Kit
+		CSOEconItemAttribute* StickerKitAttribute = Skin.add_attribute();
+		uint32_t StickerKitAttributeValue = 0;
+
+		StickerKitAttribute->set_def_index(113 + 4 * j);
+
+		StickerKitAttribute->set_value_bytes(&StickerKitAttributeValue, 4);
+
+		// Sticker Wear
+		CSOEconItemAttribute* StickerWearAttribute = Skin.add_attribute();
+		float StickerWearAttributeValue = 0.001f;
+
+		StickerWearAttribute->set_def_index(114 + 4 * j);
+
+		StickerWearAttribute->set_value_bytes(&StickerWearAttributeValue, 4);
+
+		// Sticker Scale
+		CSOEconItemAttribute* StickerScaleAttribute = Skin.add_attribute();
+		float StickerScaleAttributeValue = 1.f;
+
+		StickerScaleAttribute->set_def_index(115 + 4 * j);
+
+		StickerScaleAttribute->set_value_bytes(&StickerScaleAttributeValue, 4);
+
+		// Sticker Rotation
+		CSOEconItemAttribute* StickerRotationAttribute = Skin.add_attribute();
+		float StickerRotationAttributeValue = 0.f;
+
+		StickerRotationAttribute->set_def_index(116 + 4 * j);
+
+		StickerRotationAttribute->set_value_bytes(&StickerRotationAttributeValue, 4);
 	}
-	if (MessageType == k_EMsgGCClientWelcome) {
-		CMsgClientWelcome Message;
-		try
-		{
-			if (!Message.ParsePartialFromArray((void*)((DWORD)pubDest + 8), *pcubMsgSize - 8))
-				return;
-		}
-		catch (...)
-		{
-			return;
-		}
-		if (Message.outofdate_subscribed_caches_size() <= 0)
-			return;
-		CMsgSOCacheSubscribed* Cache = Message.mutable_outofdate_subscribed_caches(0);
-		for (int i = 0; i < Cache->objects_size(); i++)
-		{
-			CMsgSOCacheSubscribed::SubscribedType* Object = Cache->mutable_objects(i);
-			if (!Object->has_type_id())
-				continue;
-			if (Object->type_id() == 1) {
-				for (int j = 0; j < Object->object_data_size(); j++)
-				{
-					std::string* ObjectData = Object->mutable_object_data(j);
-					CSOEconItem Item;
-					if (!Item.ParseFromArray((void*)const_cast<char*>(ObjectData->data()), ObjectData->size()))
-						continue;
-					if (Item.equipped_state_size() <= 0)
-						continue;
-					if (Settings::MedalChanger::equipped_medal_override)
-					{
-						CSOEconItemEquipped* EquippedState = Item.mutable_equipped_state(0);
-						if (EquippedState->new_class() == 0 && EquippedState->new_slot() == 55)
-						{
-							Item.clear_equipped_state();
-							ObjectData->resize(Item.ByteSize());
-							Item.SerializeToArray((void*)const_cast<char*>(ObjectData->data()), Item.ByteSize());
-						}
-					}
-				}
-				ApplyMedals(Object);
-				ApplySkins(Object);
-			}
-		}
-		if ((uint32_t)Message.ByteSize() <= cubDest - 8)
-		{
-			Message.SerializeToArray((void*)((DWORD)pubDest + 8), Message.ByteSize());
-			*pcubMsgSize = Message.ByteSize() + 8;
-		}
-	}
+
+	Object->add_object_data(Skin.SerializeAsString());
 }
-bool CInventoryChanger::PreSendMessage(uint32_t &unMsgType, void* pubData, uint32_t &cubData)
-{
-	if (!Settings::InventoryChanger::enabled)
-		return true;
-	uint32_t MessageType = unMsgType & 0x7FFFFFFF;
-	if (MessageType == k_EMsgGCAdjustItemEquippedState) {
-		CMsgAdjustItemEquippedState Message;
-		try
-		{
-			if (!Message.ParsePartialFromArray((void*)((DWORD)pubData + 8), cubData - 8))
-				return true;
-		}
-		catch (...)
-		{
-			return true;
-		}
-		if (!Message.has_item_id() || !Message.has_new_class() || !Message.has_new_slot())
-			return true;
-		uint32_t item_id = (uint32_t)Message.item_id() - 20000;
-		if (item_id < 1 || item_id > Settings::InventoryChanger::weapons.size()) {
-			return true;
-		}
-		auto weapon = Settings::InventoryChanger::weapons[item_id - 1];
-		g_SkinChangerCfg[weapon.itemDefinitionIndex].flFallbackWear = weapon.wear;
-		g_SkinChangerCfg[weapon.itemDefinitionIndex].iItemDefinitionIndex = weapon.itemDefinitionIndex;
-		g_SkinChangerCfg[weapon.itemDefinitionIndex].nFallbackPaintKit = weapon.paintKit;
-		ForceFullUpdate();
-		return false;
-	}
-	return true;
-}
-void CInventoryChanger::ApplySkins(CMsgSOCacheSubscribed::SubscribedType* pInventoryCacheObject)
-{
-	if (!Settings::InventoryChanger::enabled) {
-		return;
-	}
+void AddSkins(CMsgSOCacheSubscribed::SubscribedType* pInventoryCacheObject) {
 	int c = 20001;
-	for (auto weapon : Settings::InventoryChanger::weapons) {
+	for (auto weapon : Settings::InvChanger::weapons) {
 		CSOEconItem Skin;
+
 		Skin.set_id(c);
 		Skin.set_account_id(Interfaces::SteamUser()->GetSteamID().GetAccountID());
 		Skin.set_def_index(weapon.itemDefinitionIndex);
@@ -147,51 +108,250 @@ void CInventoryChanger::ApplySkins(CMsgSOCacheSubscribed::SubscribedType* pInven
 		Skin.set_original_id(0);
 		Skin.set_rarity(weapon.rarity);
 		Skin.set_quality(0);
+
 		auto PaintKitAttribute = Skin.add_attribute();
 		float PaintKitAttributeValue = (float)weapon.paintKit;
 		PaintKitAttribute->set_def_index(6);
 		PaintKitAttribute->set_value_bytes(&PaintKitAttributeValue, 4);
+
 		auto SeedAttribute = Skin.add_attribute();
 		float SeedAttributeValue = (float)weapon.seed;
 		SeedAttribute->set_def_index(7);
 		SeedAttribute->set_value_bytes(&SeedAttributeValue, 4);
+
 		auto WearAttribute = Skin.add_attribute();
 		float WearAttributeValue = weapon.wear;
 		WearAttribute->set_def_index(8);
 		WearAttribute->set_value_bytes(&WearAttributeValue, 4);
+		for (int j = 0; j < 4; j++)
+		{
+			// Sticker Kit
+			CSOEconItemAttribute* StickerKitAttribute = Skin.add_attribute();
+			uint32_t StickerKitAttributeValue = 0;
+
+			StickerKitAttribute->set_def_index(113 + 4 * j);
+
+			StickerKitAttribute->set_value_bytes(&StickerKitAttributeValue, 4);
+
+			// Sticker Wear
+			CSOEconItemAttribute* StickerWearAttribute = Skin.add_attribute();
+			float StickerWearAttributeValue = 0.001f;
+
+			StickerWearAttribute->set_def_index(114 + 4 * j);
+
+			StickerWearAttribute->set_value_bytes(&StickerWearAttributeValue, 4);
+
+			// Sticker Scale
+			CSOEconItemAttribute* StickerScaleAttribute = Skin.add_attribute();
+			float StickerScaleAttributeValue = 1.f;
+
+			StickerScaleAttribute->set_def_index(115 + 4 * j);
+
+			StickerScaleAttribute->set_value_bytes(&StickerScaleAttributeValue, 4);
+
+			// Sticker Rotation
+			CSOEconItemAttribute* StickerRotationAttribute = Skin.add_attribute();
+			float StickerRotationAttributeValue = 0.f;
+
+			StickerRotationAttribute->set_def_index(116 + 4 * j);
+
+			StickerRotationAttribute->set_value_bytes(&StickerRotationAttributeValue, 4);
+		}
+
 		pInventoryCacheObject->add_object_data(Skin.SerializeAsString());
+
 		c++;
 	}
 }
-void CInventoryChanger::ApplyMedals(CMsgSOCacheSubscribed::SubscribedType* pInventoryCacheObject)
+void CInventoryChanger1::PostRetrieveMessage(uint32_t* punMsgType, void* pubDest, uint32_t cubDest, uint32_t* pcubMsgSize)  // RANK CHANGER
 {
-	if (!Settings::MedalChanger::enabled) {
+	uint32_t MessageType = *punMsgType & 0x7FFFFFFF;
+
+	if (MessageType == k_EMsgGCCStrike15_v2_MatchmakingGC2ClientHello)
+	{
+		CMsgGCCStrike15_v2_MatchmakingGC2ClientHello Message;
+		try
+		{
+			if (!Message.ParsePartialFromArray((void*)((DWORD)pubDest + 8), *pcubMsgSize - 8))
+				return;
+		}
+		catch (...)
+		{
+			return;
+		}
+
+		Message.set_player_level(Settings::InvChanger::Profile_Info_Level); // PLAYER LEVEL
+		Message.set_player_cur_xp(Settings::InvChanger::Profile_Info_XP); // PLAYER XP
+
+																		  // TournamentPlayer
+
+		PlayerRankingInfo* Ranking = Message.mutable_ranking();
+		Ranking->set_account_id(Interfaces::SteamUser()->GetSteamID().GetAccountID());
+		Ranking->set_rank_id(Settings::InvChanger::Profile_Info_Rank); // PLAYER RANK 1-18 (18 = Global Elite)
+		Ranking->set_wins(Settings::InvChanger::Profile_Info_Win);  // PLAYER WINS IN MATCHMAKING
+
+		PlayerCommendationInfo* Commendation = Message.mutable_commendation();
+		Commendation->set_cmd_friendly(Settings::InvChanger::Profile_Info_Friendly);  //RECOMMENDATIONS FRIENDLY
+		Commendation->set_cmd_leader(Settings::InvChanger::Profile_Info_Leader);    //RECOMMENDATIONS LEADER
+		Commendation->set_cmd_teaching(Settings::InvChanger::Profile_Info_Teacher);  //RECOMMENDATIONS TEACHING
+
+		if ((uint32_t)Message.ByteSize() <= cubDest - 8)
+		{
+			Message.SerializeToArray((void*)((DWORD)pubDest + 8), Message.ByteSize());
+
+			*pcubMsgSize = Message.ByteSize() + 8;
+		}
+	}
+}
+
+void CInventoryChanger::PostRetrieveMessage(uint32_t* punMsgType, void* pubDest, uint32_t cubDest, uint32_t* pcubMsgSize)
+{
+	uint32_t MessageType = *punMsgType & 0x7FFFFFFF;
+
+	if (MessageType != k_EMsgGCClientWelcome)
+		return;
+	CMsgClientWelcome Message;
+
+	try
+	{
+		if (!Message.ParsePartialFromArray((void*)((DWORD)pubDest + 8), *pcubMsgSize - 8))
+			return;
+	}
+	catch (...)
+	{
 		return;
 	}
-	int c = 10001;
-	for (uint32_t MedalIndex : Settings::MedalChanger::medals)
+
+	if (Message.outofdate_subscribed_caches_size() <= 0)
+		return;
+
+	CMsgSOCacheSubscribed* Cache = Message.mutable_outofdate_subscribed_caches(0);
+
+	for (int i = 0; i < Cache->objects_size(); i++)
+	{
+		CMsgSOCacheSubscribed::SubscribedType* Object = Cache->mutable_objects(i);
+
+		if (!Object->has_type_id())
+			continue;
+
+		if (Object->type_id() == 1) {
+			for (int j = 0; j < Object->object_data_size(); j++)
+			{
+				std::string* ObjectData = Object->mutable_object_data(j);
+
+				CSOEconItem Item;
+
+				if (!Item.ParseFromArray((void*)const_cast<char*>(ObjectData->data()), ObjectData->size()))
+					continue;
+
+				if (Item.equipped_state_size() <= 0)
+					continue;
+				if (Settings::InvChanger::MedalOverride_enable)
+				{
+					CSOEconItemEquipped* EquippedState = Item.mutable_equipped_state(0);
+
+					if (EquippedState->new_class() == 0 && EquippedState->new_slot() == 55)
+					{
+						Item.clear_equipped_state();
+
+						ObjectData->resize(Item.ByteSize());
+						Item.SerializeToArray((void*)const_cast<char*>(ObjectData->data()), Item.ByteSize());
+					}
+				}
+			}
+			if (Settings::InvChanger::Inventory_Changer_Medal)
+				ApplyMedals(Object);
+			if (Settings::InvChanger::Inventory_Changer)
+				AddSkins(Object);
+
+		}
+	}
+
+	if ((uint32_t)Message.ByteSize() <= cubDest - 8)
+	{
+		Message.SerializeToArray((void*)((DWORD)pubDest + 8), Message.ByteSize());
+
+		*pcubMsgSize = Message.ByteSize() + 8;
+	}
+}
+
+bool CInventoryChanger1::PreSendMessage(uint32_t &unMsgType, void* pubData, uint32_t &cubData)
+{
+	uint32_t MessageType = unMsgType & 0x7FFFFFFF;
+	if (MessageType == k_EMsgGCAdjustItemEquippedState) {
+		CMsgAdjustItemEquippedState Message;
+
+		try
+		{
+			if (!Message.ParsePartialFromArray((void*)((DWORD)pubData + 8), cubData - 8))
+				return true;
+		}
+		catch (...)
+		{
+			return true;
+		}
+
+		if (!Message.has_item_id() || !Message.has_new_class() || !Message.has_new_slot())
+			return true;
+
+		uint32_t item_id = (uint32_t)Message.item_id() - 20000;
+
+		if (item_id < 1 || item_id > Settings::InvChanger::weapons.size()) {
+			return true;
+		}
+
+		auto weapon = Settings::InvChanger::weapons[item_id - 1];
+		g_SkinChangerCfg[weapon.itemDefinitionIndex].flFallbackWear = weapon.wear;
+		g_SkinChangerCfg[weapon.itemDefinitionIndex].iItemDefinitionIndex = weapon.itemDefinitionIndex;
+		g_SkinChangerCfg[weapon.itemDefinitionIndex].nFallbackPaintKit = weapon.paintKit;
+
+		ForceFullUpdate();
+
+		return false;
+	}
+	return true;
+}
+
+bool CInventoryChanger::PreSendMessage(uint32_t &unMsgType, void* pubData, uint32_t &cubData)
+{
+	uint32_t MessageType = unMsgType & 0x7FFFFFFF;
+
+	return true;
+}
+
+void CInventoryChanger::ApplyMedals(CMsgSOCacheSubscribed::SubscribedType* pInventoryCacheObject)
+{
+	int i = 10001;
+	for (uint32_t MedalIndex : Settings::InvChanger::medals)
 	{
 		CSOEconItem Medal;
+
 		Medal.set_account_id(Interfaces::SteamUser()->GetSteamID().GetAccountID());
 		Medal.set_origin(9);
 		Medal.set_rarity(6);
 		Medal.set_quantity(1);
 		Medal.set_quality(4);
 		Medal.set_level(1);
+
 		CSOEconItemAttribute* TimeAcquiredAttribute = Medal.add_attribute();
+		int64_t ms = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 		uint32_t TimeAcquiredAttributeValue = 0;
+
 		TimeAcquiredAttribute->set_def_index(222);
 		TimeAcquiredAttribute->set_value_bytes(&TimeAcquiredAttributeValue, 4);
+
 		Medal.set_def_index(MedalIndex);
-		Medal.set_inventory(c);
-		Medal.set_id(c);
-		if (Settings::MedalChanger::equipped_medal_override && Settings::MedalChanger::equipped_medal == MedalIndex)
+		Medal.set_inventory(i);
+		Medal.set_id(i);
+		if (Settings::InvChanger::MedalOverride_enable && Settings::InvChanger::MedalOverride == MedalIndex)
 		{
 			CSOEconItemEquipped* EquippedState = Medal.add_equipped_state();
+
 			EquippedState->set_new_class(0);
 			EquippedState->set_new_slot(55);
 		}
 		pInventoryCacheObject->add_object_data(Medal.SerializeAsString());
-		c++;
+
+		i++;
 	}
 }
