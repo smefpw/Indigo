e693c9f8bc9ac0459a1124049fe8fe85a81b93a0
justin <smefpw@gmail.com>
2019-11-29 13:14:28 +0000

    Update Sounds.h

--- INDIGO/SDK/Sounds.h
+++ INDIGO/SDK/Sounds.h
@@ -3,5 +3,5 @@ namespace SDK
 {
 	extern const char cod[11752]; // default (cod hitsound)
 	extern const char bameware[62840]; // bameware
-	extern const char penguware[]; // penguware
+	extern const char penguware[8092]; // penguware
 }
